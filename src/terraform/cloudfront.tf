4cab8d1e7fff38e2138e4b304180ec6b0052d039
ki225 <271yeye@gmail.com>
2025-04-26 01:47:04 +0800

    feat: cloudfront for gateway

---
+++ src/terraform/cloudfront.tf
@@ -0,0 +1,101 @@
+resource "aws_cloudfront_distribution" "main" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Equipment Management Application"
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100" # 僅北美和歐洲
+  web_acl_id          = aws_wafv2_web_acl.main.arn
+
+  # API Gateway Origin - only for API Gateway
+  origin {
+    domain_name = replace(aws_apigatewayv2_api.main.api_endpoint, "https://", "")
+    origin_id   = "ApiGateway"
+    
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  #all the requests will be sent to the API Gateway
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "ApiGateway"
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Authorization", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = true
+  }
+
+  # improve the performance of the website by caching static files
+  ordered_cache_behavior {
+    path_pattern     = "/api/images/*" 
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "ApiGateway"
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400    # 1 天
+    max_ttl                = 31536000 # 1 年
+    compress               = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/api/reports/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "ApiGateway"
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Origin"]
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600     # 1 小時
+    max_ttl                = 86400    # 1 天
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  # SSL certificate for CloudFront
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Environment = var.environment
+    Project     = "hackthon api"
+  }
+}

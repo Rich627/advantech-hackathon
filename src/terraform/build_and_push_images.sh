70a8037dfd2be904277bf2e2ee55281675ab7e77
yylunxie <yylunxie@gmail.com>
2025-04-27 02:02:27 +0800

    feat: (doc_procee) simplify Lambda to accept uploaded PDF and save to S3 with original filename

--- src/terraform/build_and_push_images.sh
+++ src/terraform/build_and_push_images.sh
@@ -4,14 +4,14 @@
 AWS_REGION="us-west-2"
 ECR_URL="467392743157.dkr.ecr.us-west-2.amazonaws.com"
 LAMBDA_FUNCTIONS=(
-  "llm_issue_handler"
+  # "llm_issue_handler"
   "doc_process"
-  "util"
-  "sns_handler"
-  "pdf_ingest_handler"
-  "render_frontend"
-  "complete"
-  "presigned_url"
+  # "util"
+  # "sns_handler"
+  # "pdf_ingest_handler"
+  # "render_frontend"
+  # "complete"
+  # "presigned_url"
 )
 NO_RETRY=${NO_RETRY:-false}  # 新增: 控制是否禁用重試機制的環境變量
 
@@ -98,9 +98,9 @@ build_and_push() {
     if docker push "$ECR_URL/$function_name:latest"; then
       echo "成功將 $function_name 推送到 ECR"
       # 更新 Lambda function
-      echo "更新 Lambda function presigned_url_generator 使用最新 ECR image..."
+      echo "更新 Lambda function doc_process 使用最新 ECR image..."
       aws lambda update-function-code \
-        --function-name "presigned_url_generator" \
+        --function-name "doc_process" \
         --image-uri "$ECR_URL/$function_name:latest" \
         --region "$AWS_REGION" | cat
     else
@@ -122,9 +122,9 @@ build_and_push() {
         echo "成功將 $function_name 推送到 ECR"
         push_success=true
         # 更新 Lambda function
-        echo "更新 Lambda function presigned_url_generator 使用最新 ECR image..."
+        echo "更新 Lambda function doc_process 使用最新 ECR image..."
         aws lambda update-function-code \
-          --function-name "presigned_url_generator" \
+          --function-name "doc_process" \
           --image-uri "$ECR_URL/$function_name:latest" \
           --region "$AWS_REGION" | cat
       else
